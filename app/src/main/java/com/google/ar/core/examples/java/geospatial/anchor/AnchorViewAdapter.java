a9992000fdfdd069257f28023704782bd68b0ee9
ReikoTK <99812512+ReikoTK@users.noreply.github.com>
2023-06-29 16:42:32 +0900

    dist calc

--- app/src/main/java/com/google/ar/core/examples/java/geospatial/anchor/AnchorViewAdapter.java
+++ app/src/main/java/com/google/ar/core/examples/java/geospatial/anchor/AnchorViewAdapter.java
@@ -1,10 +1,12 @@
 package com.google.ar.core.examples.java.geospatial.anchor;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.ar.core.examples.java.geospatial.AnchorListingActivity;
@@ -36,12 +38,23 @@ public class AnchorViewAdapter extends RecyclerView.Adapter<AnchorViewHolder>{
         viewHolder.Name = list.get(position).getName();
         act.addMarkerToMap(list.get(position).getLatitude(),list.get(position).getLongitude(),list.get(position).getName(),viewHolder.itemView);
 
-        DecimalFormat df = new DecimalFormat("0.00");
-        viewHolder.distanceView.setText(df.format(act.calcDist(list.get(position).latitude,list.get(position).longitude)) + "M");
+        Float dist = act.calcDist(list.get(position).latitude,list.get(position).longitude);
+        if (dist>1000){
+            DecimalFormat df = new DecimalFormat("0.0");
+            viewHolder.distanceView.setText(df.format(dist) + "KM");
+        }else{
+            DecimalFormat df = new DecimalFormat("0.00");
+            viewHolder.distanceView.setText(df.format(dist) + "M");
+        }
     }
 
     @Override
     public int getItemCount(){
         return list.size();
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(AnchorViewHolder holder) {
+        //holder.itemView.findViewById(R.id.LLClickable).setBackgroundColor(Color.argb(1,0.19f,0.19f,0.19f));
     }
 }
